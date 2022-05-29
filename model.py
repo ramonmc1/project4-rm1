71ad60f60d15ddfabc95979c8937043d9382bb37
ramon <ramon@MacBook-Pro-3.local>
2022-05-29 19:53:45 -0400

    added SQLA

--- model.py
+++ model.py
@@ -1,63 +1,50 @@
-from flask import Flask, render_template, jsonify, request
-import numpy as np
-import pandas as pd
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from config import p_key
+fimport os
+from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
-import os
-import psycopg2
-import ml_script
 
-#################################################
-# Flask Setup
-#################################################
-
-app = Flask(__name__)
-
-default_database_path= f'postgresql://postgres:{p_key}@localhost:5432/housing3'
-database_path = os.getenv('DATABASE_URL', default_database_path)
-SQLALCHEMY_DATABASE_URI = database_path
-
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo = False) 
-connection = engine.connect()
-
-
-#################################################
-# Data Prep
-#################################################
-
-# database_path = os.environ.get(‘DATABASE_URL’)
-
-# con = psycopg2.connect(DATABASE_URL)
-
-# DATABASE_URL = os.environ[f'postgresql://postgres:{p_key}@localhost:5432/housing3'] 
-
-# connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-
-data_base = ml_script.data_info()
-
-data1, df_elbow = ml_script.cluster_info(data_base)
-data3, data5 = ml_script.line_info(data_base)
-
-data1.to_sql('clusterA',  if_exists='replace', index=True, con=connection, method='multi')
-connection.execute('ALTER TABLE "clusterA" ADD PRIMARY KEY ("index");')
-
-df_elbow.to_sql('elbow',  if_exists='replace', index=True, con=connection, method='multi')
-connection.execute('ALTER TABLE "elbow" ADD PRIMARY KEY ("index");')
-
-data3.to_sql('line', if_exists='replace', index=True, con=connection, method='multi')
-connection.execute('ALTER TABLE "line" ADD PRIMARY KEY ("index");')
-
-data5.to_sql('linreg', if_exists='replace', index=True, con=connection, method='multi')
-connection.execute('ALTER TABLE "linreg" ADD PRIMARY KEY ("index");')
-
-# connection.close()
-
-
+db = SQLAlchemy()
+'''
+setup_db(app):
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app):
+    database_name ='local_db_name'
+    default_database_path= "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', database_name)
+    database_path = os.getenv('DATABASE_URL', default_database_path)
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+'''
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+'''
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+    
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), unique=True)
+    release_date = Column(db.DateTime)
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+    def details(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+        }
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
 
 #################################################
 # Database Setup
